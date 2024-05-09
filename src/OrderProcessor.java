8bc00d0350eac79d624b357f5c99fccfc843de65
Sam Rose <100201980+srose99@users.noreply.github.com>
2024-05-09 13:32:53 +0100

    Add files via upload

---
+++ src/OrderProcessor.java
@@ -0,0 +1,48 @@
+package eCommClover;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrderProcessor {
+    
+    private Map<String, Integer> inventory;
+    
+    public OrderProcessor() {
+        inventory = new HashMap<>();
+        inventory.put("item1", 10);
+        inventory.put("item2", 5);
+        inventory.put("item3", 20);
+    }
+    
+    public boolean processOrder(String item, int quantity) {
+        if (inventory.containsKey(item)) {
+            int availableQuantity = inventory.get(item);
+            if (availableQuantity >= quantity) {
+                inventory.put(item, availableQuantity - quantity);
+                System.out.println("Order processed: " + quantity + " " + item + "(s)");
+                return true;
+            } else {
+                System.out.println("Insufficient quantity available for " + item);
+                return false;
+            }
+        } else {
+            System.out.println("Item " + item + " not found in inventory");
+            return false;
+        }
+    }
+    
+    public void updateInventory(String item, int quantity) {
+        if (inventory.containsKey(item)) {
+            int currentQuantity = inventory.get(item);
+            inventory.put(item, currentQuantity + quantity);
+            System.out.println("Inventory updated for " + item + ": " + quantity);
+        } else {
+            inventory.put(item, quantity);
+            System.out.println("New item added to inventory: " + item + ": " + quantity);
+        }
+    }
+    
+    public Map<String, Integer> getInventory() {
+        return inventory;
+    }
+}
